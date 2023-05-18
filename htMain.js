947f868b06a1df4dcbfa1eab0b6fda9793fdb98e
hw130 <hewon130@gmail.com>
2023-05-18 23:50:44 +0900

    htMain

---
+++ htMain.js
@@ -0,0 +1,15 @@
+const humitemp = require('./humitemp.js');
+const HTPIN = 21;
+
+humitemp.init(HTPIN);
+
+console.log("======================================");
+console.log("3초후부터 3초간격으로 온도와 습도를 측정합니다.");
+console.log("======================================");
+
+setInterval(() => {humitemp.read();}, 3000);
+
+process.on('SIGINT', fuinction(){
+    humitemp.Disable();
+
+})
